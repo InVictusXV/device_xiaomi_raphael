3fd463ac5d4db4ad3e5d78cfa1e9b0e101ef86c3
Bruno Martins <bgcngm@gmail.com>
2019-01-23 10:25:44 +0000

    raphael: Implement SunlightEnhancement LiveDisplay HAL
    Change-Id: I17b0215ad78d86989c146291552c5e6e7b6a689d
    
    Former-commit-id: bb1050a1241cac44d61ca5f60e1db792f45c1549
    Signed-off-by: anuragbhaumik <bhaumik463@gmail.com>

--- device.mk
+++ device.mk
@@ -294,6 +294,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_raphael
 
+# Livedisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service.raphael
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
